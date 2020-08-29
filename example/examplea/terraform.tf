95dbdea02f2d4dc137880135cb8dee4df19c32eb
James Woolfenden <James.Woolfenden@gmail.com>
2020-08-29 17:01:40 +0100

    fix for tf 0.13

---
+++ example/examplea/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "3.2.0"
+      source  = "hashicorp/aws"
+    }
+
+    template = {
+      version = "2.1"
+    }
+
+    local = {
+      version = "1.2"
+    }
+
+  }
+}
