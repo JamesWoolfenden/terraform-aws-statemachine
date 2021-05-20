8af0c9233c901a4df574ce47c7a3ff55f343ae65
James Woolfenden <James.Woolfenden@gmail.com>
2021-05-20 07:59:34 +0100

    merge

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      version = "3.32.0"
+      version = "3.39.0"
       source  = "hashicorp/aws"
     }
     template = {
-      version = "2.1"
+      version = "2.2.0"
     }
 
     local = {
       version = "1.2"
     }
   }
-  required_version = "0.14.8"
+  required_version = ">=0.14.8"
 }
