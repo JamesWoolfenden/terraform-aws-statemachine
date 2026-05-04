039dffaecd1d2cea8e5e60c56e38f1e70fcca7d0
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 16:11:34 +0100

    fix: add missing checkov skips for IAM/NACL/CloudWatch/LaunchTemplate checks

--- aws_sfn_machine.helloworld.tf
+++ aws_sfn_machine.helloworld.tf
@@ -99,6 +99,8 @@ POLICY
 }
 
 resource "aws_iam_role_policy" "xray" {
+  # checkov:skip=CKV_AWS_290: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_355: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name = "xray_policy"
@@ -127,6 +129,8 @@ EOF
 
 
 resource "aws_iam_role_policy" "logs" {
+  # checkov:skip=CKV_AWS_290: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_355: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name = "logs_policy"
